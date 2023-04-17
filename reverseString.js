ae88c35b8b85957565bf8b7fdeaa6c29ec7bb69b
James Njenga <knjenga94@gmail.com>
2023-04-18 00:20:01 +0300

    Modify reverse string code

--- reverseString.js
+++ reverseString.js
@@ -1,7 +1,13 @@
-let myString = "James"
-let toArray=myString.split(",")
+let myString = "Control"
+let toArray = myString.split("")
 //console.log(toArray)
 
-for(i=0;i<=toArray.length;i++){
-    
+let temp = ""
+
+for (i = 0; i < toArray.length; i++) {
+
+    // console.log(toArray[i])
+    temp = toArray[i]+temp
 }
+
+console.log(temp)
